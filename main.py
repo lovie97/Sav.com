cee593ce458f30af273dda0180dcccb683216a2d
Luv Patel <lovie97@gmail.com>
2021-02-21 20:41:55 -0600

    Create main.py
    **Python Software Engineer coding challlenge**

---
+++ main.py
@@ -0,0 +1,28 @@
+import socket
+
+def doSearch(server, request):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((server, 43))
+    s.send((request + '\r\n').encode())
+
+    info = s.recv(300)
+    info = info.decode()
+    info = info.split("\r\n")
+    info = info[6].lstrip()
+    s.close()
+
+    return info[22:32]
+
+def getDate():
+
+    server = "whois.internic.net"
+    request = input("Enter a .com domain: ")
+
+    if "http://" in request:
+        request = request.replace("http://","")
+    if "www." in request:
+        request = request.replace("www.","")
+
+    print("Domain expiration date: " + doSearch(server, request))
+
+getDate()
